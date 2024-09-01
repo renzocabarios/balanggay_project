51660db2136448e179faa4e35470322d46d34cf4
renzocabarios <renzo.cabarios@gmail.com>
2024-09-01 17:00:58 +0800

    Update blueprints

---
+++ blueprints/README.md
@@ -0,0 +1,27 @@
+```
+-- DB = {
+--   DECKS = 0,
+--   USER_DECKS = 0,
+--   USER_CARDS = 0,
+--   USERS = 0,
+--   CARDS = 0
+-- }
+
+-- Decks = {}
+-- Cards = {}
+-- Users = {}
+-- UserDecks = {}
+-- UserCards = {}
+
+
+CreateDeck
+-- Send({ Target = "R9aWm3slNiWeH4ToQV8iCFteeuDx-IvKx2HWQLMOG0g", Data = '{"name":"qwdqd","description":"test"}', Action = "CreateDeck"})
+
+
+CreateUser
+-- Send({ Target = "R9aWm3slNiWeH4ToQV8iCFteeuDx-IvKx2HWQLMOG0g", Data = '{}', Action = "CreateUser"})
+
+
+`.load init.lua`
+
+```
--- blueprints/init.lua
+++ blueprints/init.lua
@@ -113,6 +113,7 @@ local json = require('json')
 
 -- User {
 --   id: number,
+--   address: string,
 --   createdAt: Date | number,
 --   updatedAt: Date | number,
 -- }
@@ -152,31 +153,125 @@ DB = DB or {
 
 Decks = Decks or {}
 Cards = Cards or {}
-
 Users = Users or {}
 UserDecks = UserDecks or {}
 UserCards = UserCards or {}
 
 
+
+
+local function createUserDeck(author, deck_id, name, description, createdAt, updatedAt)
+  DB["USER_DECKS"] = DB["USER_DECKS"] + 1
+  table.insert(UserDecks, {
+    id = DB["USER_DECKS"],
+    deck_id = deck_id,
+    author = author,
+    name = name,
+    description = description,
+    listed = false,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+  })
+end
+
+local function createDeck(author, name, description, createdAt, updatedAt)
+  DB["DECKS"] = DB["DECKS"] + 1
+  table.insert(Decks, {
+    id = DB["DECKS"],
+    author = author,
+    name = name,
+    description = description,
+    listed = false,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+  })
+end
+
+local function createUser(address, createdAt, updatedAt)
+  DB["USERS"] = DB["USERS"] + 1
+  table.insert(Users, {
+    id = DB["USERS"],
+    address = address,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+  })
+end
+
 -- Handlers
+
 Handlers.add(
   "create_deck",
   Handlers.utils.hasMatchingTag("Action", "CreateDeck"),
   function(msg)
     local data = json.decode(msg.Data)
-    DB["DECKS"] = DB["DECKS"] + 1
-    table.insert(Decks, {
-      id = DB["DECKS"],
-      author = msg.From,
-      name = data.name,
-      description = data.description,
-      listed = false,
-      createdAt = msg.Timestamp,
-      updatedAt = msg.Timestamp,
-    })
+
+    local userExists = false
+    for _, account in ipairs(Users) do
+      if account.address == msg.From then
+        userExists = true
+        break
+      end
+    end
+
+    if userExists == false then
+      createUser(
+        msg.From,
+        msg.Timestamp,
+        msg.Timestamp
+      )
+    end
+
+    createDeck(
+      msg.From,
+      data.name,
+      data.description,
+      msg.Timestamp,
+      msg.Timestamp
+    )
+
+    createUserDeck(
+      msg.From,
+      DB["DECKS"],
+      data.name,
+      data.description,
+      msg.Timestamp,
+      msg.Timestamp
+    )
+
     Handlers.utils.reply("create deck success")(msg)
   end
 )
+
+Handlers.add(
+  "create_user",
+  Handlers.utils.hasMatchingTag("Action", "CreateUser"),
+  function(msg)
+    local userExists = false
+    for _, account in ipairs(Users) do
+      if account.address == msg.From then
+        userExists = true
+        break
+      end
+    end
+
+    if userExists == false then
+      createUser(
+        msg.From,
+        msg.Timestamp,
+        msg.Timestamp
+      )
+    else
+      Handlers.utils.reply("create user failed")(msg)
+      return
+    end
+
+    Handlers.utils.reply("create user success")(msg)
+  end
+)
+
+-- Send({ Target = "R9aWm3slNiWeH4ToQV8iCFteeuDx-IvKx2HWQLMOG0g", Data = '{"name":"qwdqd","description":"test"}', Action = "CreateUser"}).receive().Data
+
+
 
 Handlers.add(
   "create_card",
@@ -252,6 +347,7 @@ Send({ Target = "R9aWm3slNiWeH4ToQV8iCFteeuDx-IvKx2HWQLMOG0g", Action = "GetMyDe
 -- Send({ Target = "s4WFjIc4ihTWN0zec7sRqDjV_ibKNsMftl-OLxDJUOI", Data = '{"name":"qwdqd","description":"test"}', Action = "CreateDeck"}).receive().Data
 -- Send({ Target = "s4WFjIc4ihTWN0zec7sRqDjV_ibKNsMftl-OLxDJUOI", Action = "GetMyDecks"}).receive().Data
 -- Send({ Target = Morpheus, Data = "Code: rabbithole", Action = "Unlock" }).receive().Data
+
 
 
 -- Instructions Needed
